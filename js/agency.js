b125db7d49cce1223a160699b697f592944c38ed
deeflash <deeflash211@gmail.com>
2016-08-20 14:09:07 +0300

    другая кнопка  с видео картигка

--- js/agency.js
+++ js/agency.js
@@ -30,9 +30,10 @@ function toggleVideo(state) {
     // if state == 'hide', hide. Else: show video
     var div = document.getElementById("popupVid");
     var iframe = div.getElementsByTagName("iframe")[0].contentWindow;
-    div.style.display = state == 'hide' ? 'none' : '';
+    var btn = $('#video-button');
+    btn.hide();
+    $(div).show(300);
+    // div.style.display = state == 'hide' ? 'none' : '';
     func = state == 'hide' ? 'pauseVideo' : 'playVideo';
     iframe.postMessage('{"event":"command","func":"' + func + '","args":""}','*');
-    var btn = $('#video-button');
-    btn.hide()
 }
